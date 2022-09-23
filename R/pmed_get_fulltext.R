792f9a8ec31ce37c1dff3c8a1334d2059f8aacb1
jaytimm <jason.timm@zohomail.com>
2022-09-23 11:12:49 -0600

    re

---
+++ R/pmed_get_fulltext.R
@@ -0,0 +1,71 @@
+#' Get full text articles from Open Access subset of PMC --
+#'
+#' @name pmed_get_fulltext
+#' @param x A character vector, namely, a file path included in OA list file
+#' @return A dtaframe
+#'
+#' @export
+#' @rdname pmed_get_fulltext
+#'
+pmed_get_fulltext <- function(x) {
+  
+  flist <- lapply(1:length(x), function(q){
+    
+    fn <- paste0('https://ftp.ncbi.nlm.nih.gov/pub/pmc/', x[q])
+    download.file(fn, destfile = "tmp.tar.gz")
+    xmls <- grep('xml$', untar("tmp.tar.gz", list = TRUE), value = T)
+    untar("tmp.tar.gz", files = xmls)
+    
+    x0 <- xml2::read_xml(xmls)
+    
+    if(length(xml2::xml_children(x0)) == 1){} else{
+      
+      x1 <- xml2::xml_child(x0, 2)
+      
+      header_titles <- lapply(xml2::xml_children(x1),
+                              function(x) {
+                                xml2::xml_text(xml2::xml_find_first(x, ".//title"))}
+      )
+      
+      # sub_titles <- lapply(xml2::xml_children(x1),
+      #                      function(x) {
+      #                        xml2::xml_text(xml2::xml_find_all(x, ".//title"))}
+      # )
+      # 
+      # toc <- setNames(stack(setNames(sub_titles, header_titles)), c('sub', 'header'))
+      # toc0 <- subset(toc, sub != header)
+      # 
+      text <- lapply(xml2::xml_children(x1), xml2::xml_text)
+      pmcid <- gsub('(^.*)(PMC)([0-9]*)(\\.tar\\.gz$)', '\\3', x[q])
+      section <- unlist(header_titles)
+      
+      df <- data.frame(doc_id = paste0(pmcid, '_', section),
+                       pmcid, 
+                       section,
+                       text = unlist(text),
+                       row.names = NULL)
+      
+      
+      # heads <- paste0('^', df$section, collapse = '|')
+      # heads <- gsub('\\(', '\\\\(', heads)
+      # heads <- gsub('\\)', '\\\\)', heads)
+      # 
+      # df$text <- gsub(heads, '', df$text)
+      
+      df$text <- gsub('([a-z]+)([A-Z])', '\\1\n\\2', df$text)
+      
+      # if (nrow(toc0) > 0) {
+      #   splits <- paste0(toc$sub, collapse = '|')
+      #   # splits <- gsub('\\(', '\\\\(', splits)
+      #   # splits <- gsub('\\)', '\\\\)', splits)
+      #   
+      #   #q <- paste0('(', splits, ')')
+      #   
+      #   df$text <- gsub(splits, '\n\n\\1: ', df$text)
+      # }
+      
+      df }
+  })
+  
+  flist |> data.table::rbindlist()
+}
