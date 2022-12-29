9a42bc43e1df0325b341a3544d81e1e58dbc4e1a
jaytimm <jason.timm@zohomail.com>
2022-12-29 09:41:49 -0700

    re

--- R/data_mesh_embeddings.R
+++ R/data_mesh_embeddings.R
@@ -5,31 +5,57 @@
 #' @rdname data_mesh_embeddings
 data_mesh_embeddings <- function() {
   
-  source_file = 'https://github.com/jaytimm/pubmedr/blob/main/download/mesh_embeddings.rds?raw=true'
-  destination_file = file.path(rappdirs::user_data_dir('pubmedr'), 'mesh_embeddings.rds')
+  sf = 'https://github.com/jaytimm/mesh-builds/blob/main/data/data_mesh_embeddings.rds?raw=true'
+  sf2 = 'https://github.com/jaytimm/mesh-builds/blob/main/data/data_scr_embeddings.rds?raw=true'
+  df = file.path(rappdirs::user_data_dir('pubmedr'), 'data_mesh_emeddings.rds')
+  df2 = file.path(rappdirs::user_data_dir('pubmedr'), 'data_scr_emeddings.rds')
   
-  if (!file.exists(destination_file)) {
+  
+  if (!file.exists(df)) {
     if (!dir.exists(rappdirs::user_data_dir('pubmedr'))) {
-      dir.create(rappdirs::user_data_dir('pubmedr'), recursive = TRUE)
+      dir.create(rappdirs::user_data_dir('pubmedr'), 
+                 recursive = TRUE)
     }
   }
   
-  if (!file.exists(destination_file)) {
+  if (!file.exists(df)) {
       
       out <- tryCatch(
         
         {
-          message('Downloading the mesh_embeddings dataset...')
-          utils::download.file(source_file, destination_file)
+          message('Downloading the mesh embeddings ...')
+          utils::download.file(sf, df)
           },
         
         error = function(e) paste("Error"))
       
       if(out == 'Error'){
         message('Download not completed ... Try options(timeout = 600)')
-        file.remove(destination_file)
+        file.remove(df)
         }
-      }else{
-        readRDS(destination_file)
       }
+  
+  if (!file.exists(df2)) {
+    
+    out <- tryCatch(
+      
+      {
+        message('Downloading the scr embeddings ...')
+        utils::download.file(sf2, df2)
+      },
+      
+      error = function(e) paste("Error"))
+    
+    if(out == 'Error'){
+      message('Download not completed ... Try options(timeout = 600)')
+      file.remove(df2)
+    }
+  }
+  
+  if (all(file.exists(df), file.exists(df2))){
+    
+    a1 <- readRDS(df)
+    a2 <- readRDS(df2)
+    data.table::rbindlist(list(a1, a2), fill = T)
+    }
   }
--- R/data_mesh_thesaurus.R
+++ R/data_mesh_thesaurus.R
@@ -5,17 +5,28 @@
 #' @rdname data_mesh_thesaurus
 data_mesh_thesuarus <- function() {
   
-  source_file = 'https://github.com/jaytimm/pubmedr/blob/main/download/mesh_thesaurus.rds?raw=true'
-  destination_file = file.path(rappdirs::user_data_dir('pubmedr'), 'mesh_thesuarus.rds')
+  sf = 'https://github.com/jaytimm/mesh-builds/blob/main/data/data_mesh_thesaurus.rds?raw=true'
+  sf2 = 'https://github.com/jaytimm/mesh-builds/blob/main/data/data_scr_thesaurus.rds?raw=true'
+  df = file.path(rappdirs::user_data_dir('pubmedr'), 'data_mesh_thesuarus.rds')
+  df2 = file.path(rappdirs::user_data_dir('pubmedr'), 'data_scr_thesuarus.rds')
   
-  if (!file.exists(destination_file)) {
+  if (!file.exists(df)) {
     if (!dir.exists(rappdirs::user_data_dir('pubmedr'))) {
-      dir.create(rappdirs::user_data_dir('pubmedr'), recursive = TRUE)
+      dir.create(rappdirs::user_data_dir('pubmedr'), 
+                 recursive = TRUE)
     }
     
-    message('Downloading the mesh_thesaurus dataset...')
-    utils::download.file(source_file, destination_file)
+    message('Downloading the mesh thesaurus ...')
+    utils::download.file(sf, df)
   }
+    
+  if (!file.exists(df2)) {
+    message('Downloading the supplemental concept thesaurus ...')
+    utils::download.file(sf2, df2)
+    }
   
-  readRDS(destination_file)
+  a1 <- readRDS(df)
+  a2 <- readRDS(df2)
+  data.table::rbindlist(list(a1, a2), fill = T)
+  
 }
