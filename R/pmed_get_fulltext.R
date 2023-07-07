ecdb7d95a983b16562b7a6b514714cf459d032bc
jaytimm <25044244+jaytimm@users.noreply.github.com>
2023-07-07 15:03:32 -0600

    re

--- R/pmed_get_fulltext.R
+++ R/pmed_get_fulltext.R
@@ -2,72 +2,63 @@
 #'
 #' @name pmed_get_fulltext
 #' @param x A character vector, namely, a file path included in OA list file
+#' @param output_dir A file path
 #' @return A dtaframe
 #'
 #' @export
 #' @rdname pmed_get_fulltext
 #'
-pmed_get_fulltext <- function(x) {
+pmed_get_fulltext <- function(x, output_dir = NULL) {
   
-  flist <- lapply(1:length(x), function(q){
+  flist <- list()
+
+  for(q in 1:length(x)){
     
-    ## still downloads to working directory -- !! -- 
+    
     fn <- paste0('https://ftp.ncbi.nlm.nih.gov/pub/pmc/', x[q])
-    tmp <- tempfile()
-    download.file(fn, destfile = tmp)
-    xmls <- grep('xml$', untar(tmp, list = TRUE), value = T)
-    untar(tmp, files = xmls, exdir = tempdir())
- 
-    x0 <- xml2::read_xml(paste0(tempdir(), '/', xmls))
-    
-    if(length(xml2::xml_children(x0)) == 1){} else{
-      
-      x1 <- xml2::xml_child(x0, 2)
-      
-      header_titles <- lapply(xml2::xml_children(x1),
-                              function(x) {
-                                xml2::xml_text(xml2::xml_find_first(x, ".//title"))}
-      )
-      
-      # sub_titles <- lapply(xml2::xml_children(x1),
-      #                      function(x) {
-      #                        xml2::xml_text(xml2::xml_find_all(x, ".//title"))}
-      # )
-      # 
-      # toc <- setNames(stack(setNames(sub_titles, header_titles)), c('sub', 'header'))
-      # toc0 <- subset(toc, sub != header)
-      # 
-      text <- lapply(xml2::xml_children(x1), xml2::xml_text)
-      pmcid <- gsub('(^.*)(PMC)([0-9]*)(\\.tar\\.gz$)', '\\3', x[q])
-      section <- unlist(header_titles)
-      
-      df <- data.frame(doc_id = paste0(pmcid, '_', section),
-                       pmcid, 
-                       section,
-                       text = unlist(text),
-                       row.names = NULL)
-      
-      
-      # heads <- paste0('^', df$section, collapse = '|')
-      # heads <- gsub('\\(', '\\\\(', heads)
-      # heads <- gsub('\\)', '\\\\)', heads)
-      # 
-      # df$text <- gsub(heads, '', df$text)
-      
-      df$text <- gsub('([a-z]+)([A-Z])', '\\1\n\\2', df$text)
-      
-      # if (nrow(toc0) > 0) {
-      #   splits <- paste0(toc$sub, collapse = '|')
-      #   # splits <- gsub('\\(', '\\\\(', splits)
-      #   # splits <- gsub('\\)', '\\\\)', splits)
-      #   
-      #   #q <- paste0('(', splits, ')')
-      #   
-      #   df$text <- gsub(splits, '\n\n\\1: ', df$text)
-      # }
-      
-      df }
-  })
   
-  flist |> data.table::rbindlist()
+    tmp <- tempfile()
+    dd <- tryCatch(download.file(fn, destfile = tmp), error = function(e) 'error')  
+    
+    if(dd == 'error'){} else{
+    
+        xmls <- grep('xml$', untar(tmp, list = TRUE), value = T)
+        untar(tmp, files = xmls, exdir = tempdir())
+        
+        x0 <- xml2::read_xml(paste0(tempdir(), '/', xmls)[1])
+        
+
+        if(length(xml2::xml_children(x0)) == 1){} else{
+          
+            x1 <- xml2::xml_child(x0, 2)
+            
+            header_titles <- lapply(xml2::xml_children(x1),
+                                    function(x) {
+                                      xml2::xml_text(xml2::xml_find_first(x, ".//title"))}
+            )
+            
+            text <- lapply(xml2::xml_children(x1), xml2::xml_text)
+            pmcid <- gsub('(^.*)(PMC)([0-9]*)(\\.tar\\.gz$)', '\\3', x[q])
+            section <- unlist(header_titles)
+            
+            df <- data.frame(doc_id = paste0(pmcid, '_', section),
+                             pmcid, 
+                             section,
+                             text = unlist(text),
+                             row.names = NULL)
+            
+            df$text <- gsub('([a-z]+)([A-Z])', '\\1\n\\2', df$text)
+            
+            if(!is.null(output_dir)){
+
+              saveRDS(df, paste0(output_dir, '/', pmcid, '.rds'))
+              
+            } else{ flist[[q]] <- df }}
+            
+            
+        }
+    }
+  
+  if(is.null(output_dir)){flist |> data.table::rbindlist()}
+  
 }
