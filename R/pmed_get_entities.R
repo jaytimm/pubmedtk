ffee56fa85fd7725434390bd61ea907f644c50a9
jaytimm <25044244+jaytimm@users.noreply.github.com>
2023-08-17 11:07:26 -0600

    re

--- R/pmed_get_entities.R
+++ R/pmed_get_entities.R
@@ -42,6 +42,8 @@ pmed_get_entities <- function (pmids,
 
 round(runif(1, min = 1, max = 3), digits = 1)
 
+# x ='28483577'
+# x = pubmed_ids[1]
 
 get_annotations <- function(x){
   
@@ -49,61 +51,66 @@ get_annotations <- function(x){
   
   Sys.sleep(round(runif(1, min = 0, max = 2), digits = 1))
   ## REQUEST
-  mydata <- jsonlite::stream_in(gzcon(con)) 
   
-  jj <- list()
+  mydata <- tryCatch(
+    jsonlite::stream_in(gzcon(con)),error = function(e) NA)  
   
-  for(i in 1:nrow(mydata)){
+  if(length(mydata) == 1){jj0 <- NA} else{
+  
+    jj <- list()
     
-    ## q <- mydata$passages[[i]]
-    pb1 <- mydata$passages[[i]]$annotations
-    names(pb1) <- c('title', 'abstract')
-    
-    if(any(nrow(pb1$title) == 0, is.null(nrow(pb1$title)))) {
-      pb1$title <- data.frame(tiab = 'title', 
-                              id = NA, 
-                              text = NA, 
-                              locations = NA, 
-                              identifier = NA, 
-                              type = NA)
-    } else{
+    for(i in 1:nrow(mydata)){
       
-      pb1$title <- cbind(tiab = 'title',
-                         pb1$title[, c('id', 'text', 'locations')],
-                         identifier = pb1$title$infons$identifier,
-                         type = pb1$title$infons$type)
+      ## q <- mydata$passages[[i]]
+      pb1 <- mydata$passages[[i]]$annotations
+      names(pb1) <- c('title', 'abstract')
       
+      if(any(nrow(pb1$title) == 0, is.null(nrow(pb1$title)))) {
+        pb1$title <- data.frame(tiab = 'title', 
+                                id = NA, 
+                                text = NA, 
+                                locations = NA, 
+                                identifier = NA, 
+                                type = NA)
+      } else{
+        
+        pb1$title <- cbind(tiab = 'title',
+                           pb1$title[, c('id', 'text', 'locations')],
+                           identifier = pb1$title$infons$identifier,
+                           type = pb1$title$infons$type)
+        
+      }
+      
+      
+      if(any(nrow(pb1$abstract) == 0, is.null(nrow(pb1$abstract)))) {
+        pb1$abstract <- data.frame(tiab = 'abstract',
+                                   id = NA, 
+                                   text = NA, 
+                                   locations = NA, 
+                                   identifier = NA, 
+                                   type = NA)
+      } else{
+        
+        pb1$abstract <- cbind(tiab = 'abstract',
+                              pb1$abstract[, c('id', 'text', 'locations')],
+                              identifier = pb1$abstract$infons$identifier,
+                              type = pb1$abstract$infons$type)
+      }
+      
+      jj[[i]] <- rbind(pb1$title, pb1$abstract)
     }
     
+    names(jj) <- mydata$id
+    jj0 <- jj |> data.table::rbindlist(idcol = 'pmid')
+    jj0$locations <- jj0$locations |> as.character()
+    jj0$locations <- gsub("[^[:digit:],]", "", jj0$locations)
     
-    if(any(nrow(pb1$abstract) == 0, is.null(nrow(pb1$abstract)))) {
-      pb1$abstract <- data.frame(tiab = 'abstract',
-                                 id = NA, 
-                                 text = NA, 
-                                 locations = NA, 
-                                 identifier = NA, 
-                                 type = NA)
-    } else{
-      
-      pb1$abstract <- cbind(tiab = 'abstract',
-                            pb1$abstract[, c('id', 'text', 'locations')],
-                            identifier = pb1$abstract$infons$identifier,
-                            type = pb1$abstract$infons$type)
-    }
-    
-    jj[[i]] <- rbind(pb1$title, pb1$abstract)
+    jj0[, c('start', 'length') := data.table::tstrsplit(locations, ",", fixed=TRUE)]
+    jj0[, start := as.integer(start)]
+    jj0[, end := start + as.integer(length)]
+  
+    jj0[, length := NULL]
+    jj0[, locations := NULL]
   }
-  
-  names(jj) <- mydata$id
-  jj0 <- jj |> data.table::rbindlist(idcol = 'pmid')
-  jj0$locations <- jj0$locations |> as.character()
-  jj0$locations <- gsub("[^[:digit:],]", "", jj0$locations)
-  
-  jj0[, c('start', 'length') := data.table::tstrsplit(locations, ",", fixed=TRUE)]
-  jj0[, start := as.integer(start)]
-  jj0[, end := start + as.integer(length)]
-
-  jj0[, length := NULL]
-  jj0[, locations := NULL]
   return(jj0)
 }
--- R/pmed_search_pubmed.R
+++ R/pmed_search_pubmed.R
@@ -1,75 +1,46 @@
 #' Perform basic PubMed search.
 #'
 #' @name pmed_search_pubmed
-#' @param search_term Query term as character string
-#' @param fields PubMed fields to query
-#' @param sleep In seconds.
-#' @param verbose Boolean
-#' @param retmax Max records
-#' @return A data frame of PMIDs
+#' @param query Query term as character string
+#' @return A vector of PMIDs
 #'
 #' @export
 #' @rdname pmed_search_pubmed
 #'
 #'
-pmed_search_pubmed <- function (search_term,
-                                fields = c('TIAB','MH'),
-                                verbose = TRUE,
-                                sleep = 1,
-                                retmax = 5000000,
-                                is_pubmed_syntax = F) { # max_n
+pmed_search_pubmed <- function(query) { # max_n
 
   
-  ## https://pubmed.ncbi.nlm.nih.gov/?term=Cranberry&filter=pubt.clinicaltrial
+  ps <- rentrez::entrez_search(db="pubmed",
+                               term = query,
+                               retmax=0,
+                               use_history = T)
   
-  fout <- list()
-  for(i in 1:length(search_term)){
-
-    pre_url1 <- "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?"
-    pre_url2 <- paste0("db=pubmed&retmax=",
-                       format(retmax, scientific = FALSE),
-                       "&term=")
-    strip_part <- 'https://pubmed.ncbi.nlm.nih.gov/\\?term='
-
+  if(ps$count==0) {
     
-    if(is_pubmed_syntax){
-      full_url <- paste0(pre_url1, pre_url2, gsub(strip_part, '', search_term))
-      out_search = gsub(strip_part, '', search_term)}else{
-        
-        url_term_query <- gsub(" ", "+", search_term[i], fixed = TRUE)
+    stop("No Pubmed search results")
     
-        if(is.null(fields)) {
-          fields3 <- url_term_query
-          out_search <- search_term[i]} else{
+  } else {
     
-            fields0 <- paste0('%5B', fields, '%5D')
-            fields1 <- paste0(url_term_query, fields0)
-            fields2 <- paste0(fields1, sep = '+OR+', collapse = '')
-            fields3 <- gsub('\\+OR\\+$', '', fields2)
+    start <- seq(from=0,to=ps$count,by=90000)
+    end <- c(start[-1],ps$count)
+    chunk <- end - start
+    idlist <- list()
     
-            out_search <- paste0(search_term[i],
-                                 paste0('[', fields, ']'),
-                                 collapse = ' OR ')
-            }
-        
-        full_url <- paste0 (pre_url1, pre_url2, fields3)
-        
-        }
-
-    
-    x <- httr::GET(full_url)
-    x1 <- xml2::read_xml(x)
-    x2 <- xml2::xml_find_all(x1, './/Id')
-    x3 <- xml2::xml_text(x2)
-
-    if (length(x3) == 0) {x3 <- NA}
-
-    fout[[i]] <- data.table::data.table(search_term = search_term[i], pmid = x3)
-
-    Sys.sleep(sleep)
-    if(verbose){
-      finally =  print(paste0(out_search, ': ', nrow(fout[[i]]), ' records'))
+    for(i in 1:length(start)){
+      
+      # print(paste0("Chunk ",i,": ",start[i]+1,"-",end[i]))
+      
+      Sys.sleep(0.5)
+      
+      idlist[[i]] <- rentrez::entrez_search(db="pubmed", 
+                                            term=query, retstart=start[i],
+                                            retmax=chunk[i],
+                                            WebEnv=ps$web_history$WebEnv)$ids
     }
+    
+   unlist(idlist)
+    
   }
-  return(data.table::rbindlist(fout))
+  
 }
